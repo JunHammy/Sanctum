58bc0faf2eea0799c4de82a2c38d63c86df1054d
JunHam <joonjunhan@gmail.com>
2026-07-18 03:49:50 +0800

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-VYfsL2kt.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-DgGH3qgX.js";import"./chunk-WYO6CB5R-Do5mQY8w.js";import"./chunk-ICXQ74PX-BbQIEyWO.js";import"./chunk-HOUHSVGY-BMucQDys.js";import"./chunk-Q4XR5HBZ-C792Rdrg.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-OGEWGWER-CIM3NlKs.js";import"./chunk-32BRIVSS-DK2oQ_ol.js";import"./chunk-XXDRQBXY-DoU4wU65.js";import"./chunk-VR4S4FIN-e_cIZN7v.js";import"./chunk-C7G6YPKG-BGoXumSR.js";import"./chunk-ZGVPDNZ5-CNjBpTBD.js";import"./chunk-52WLFC77-D_goyMHj.js";import"./chunk-FWX5IMBZ-nqacG4U_.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-PUDLZKDR-iHRVsQb6.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
